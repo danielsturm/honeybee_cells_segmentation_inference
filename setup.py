ac4f1e4fb2b4dd307cc63691cc54f0cf743f0016
jacobdavidson <jddavid@umich.edu>
2025-03-13 07:54:42 +0100

    include model file in setup

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
     dependency_links=dep_links,
     packages=find_packages(),
     package_dir={"honeybee_comb_inferer": "honeybee_comb_inferer/"},
-    # package_data={"honeybee_comb_inferer": ["config/config.yaml", "config/label_classes.json"]},
-    # package_data={"honeybee-comb-inferer": ["*"]},
-    # data_files=[("config-honeybee-comb", ["config/config.yaml"]), ("label-classes", ["data/label_classes.json"])],
+    include_package_data=True,
+    package_data={'': ['models/*.pth']}
 )
